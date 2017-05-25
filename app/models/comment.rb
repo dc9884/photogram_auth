3e0529d92d55d4ea7498f9f348f5357a787bf14f
David Caine <dc9884@users.noreply.github.com>
2017-05-24 23:50:57 -0500

    started using font-awesome icons

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,6 @@ validates :photo_id, :presence => true
 validates :body, :presence => true
 
 belongs_to(:user, class_name: "User", foreign_key: "user_id")
-benlongs_to :photo
+belongs_to :photo
 
 end
